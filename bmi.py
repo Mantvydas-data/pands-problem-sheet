9fde51eb528e867f5afe7b1ab74caed9ebe52d88
Mantvydas-data <G00398349@gmit.ie>
2021-03-09 11:36:15 +0000

    Adding if statements for BMI check.

--- bmi.py
+++ bmi.py
@@ -5,4 +5,13 @@ weight = float(input("Please input your weight in kg: "))
 height = int(input("Please input your height in cm: "))/100
 BMI = weight / height**2
 print("Your BMI is " + str(BMI))
-print( height)
+
+#Adding statements to also advise on BMI health ranges
+#https://www.cancer.org/cancer/cancer-causes/diet-physical-activity/body-weight-and-cancer-risk/adult-bmi.html
+if BMI < 18.5:
+    print("Your BMI is below 18.5, you could be underweight. Please consult your doctor. ")
+elif BMI == 18.5 or BMI < 24.9:
+    print("You have normal weight BMI")
+elif BMI == 25 or BMI < 29.9:
+    print("BMI of 25 to 29.9 indicates you might be overweight ")
+else : print ("High BMI suggests obesity, please consult your doctor. ")
